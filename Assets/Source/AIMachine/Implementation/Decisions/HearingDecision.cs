f69fe6b6de30f13db47fe7d9ea068d9d62d8f81f
Unknown <ivancek@gmail.com>
2018-12-12 21:25:26 +0100

    Hearing decision
    Added hearing decision to enemies.

---
+++ Assets/Source/AIMachine/Implementation/Decisions/HearingDecision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/Hearing")]
+public class HearingDecision : Decision
+{
+    public override bool Decide(AIController controller)
+    {
+        return Hear(controller);
+    }
+
+    private bool Hear(AIController controller)
+    {
+        bool isPlayerInRange = Vector3.Distance(GameInstance.GameMode.PlayerPawn.transform.position, controller.GetControlledPawn().transform.position) <= controller.enemyStats.searchRange;
+        bool isMakingNoise = ((Soldier)GameInstance.GameMode.PlayerPawn).noiseEmitter.IsEmitingNoise;
+
+        bool playerHeard = isPlayerInRange && isMakingNoise;
+
+        controller.target = playerHeard ? GameInstance.GameMode.PlayerPawn : null;
+
+        return playerHeard;
+    }
+}
